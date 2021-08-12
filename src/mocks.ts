d21aff5022ad2457e7fd71f59237dd43227e08a8
yun <nasuler@gmail.com>
2021-08-13 00:13:41 +0900

    add mock postst

---
+++ src/mocks.ts
@@ -0,0 +1,26 @@
+import moment from "moment";
+
+export interface Post {
+    id: string,
+    title: string,
+    created: moment.Moment
+}
+
+export const today: Post = {
+    id: '1',
+    title: 'Today',
+    created: moment()
+}
+
+export const thisWeek: Post = {
+    id: '2',
+    title: 'This week',
+    created: moment().subtract(2, 'days')
+}
+
+export const thisMonth: Post = {
+    id: '3',
+    title: 'This Month',
+    created: moment().subtract(12, 'days')
+}
+
