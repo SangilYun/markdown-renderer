e2bda11a3b214f73ed022f37bdec18d4a0a721cd
yun <nasuler@gmail.com>
2021-08-13 00:14:00 +0900

    add post filtering feature

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -3,17 +3,29 @@
     <span class="panel-tabs">
       <a
         v-for="period in periods"
+        :key="period"
         :class="{'is-active': period === currentPeriod}"
         @click="setPeriod(period)"
       >
         {{ period }}
       </a>
     </span>
+
+    <a
+      v-for="post in posts"
+      :key="post.id"
+      class="panel-block"
+    >
+      <a>{{ post.title }}</a>
+      <div>{{post.created.format('Do MMM')}}</div>
+    </a>
   </nav>
 </template>
 
 <script lang="ts">
-import {defineComponent, ref} from 'vue';
+import {computed, defineComponent, ref} from 'vue';
+import moment from 'moment'
+import {today, thisMonth, thisWeek} from '../mocks'
 
 type Period = 'Today' | 'This Week' | 'This Month'
 export default defineComponent({
@@ -21,10 +33,29 @@ export default defineComponent({
   setup() {
     const periods: Array<Period> = ['Today', 'This Week', 'This Month']
     const currentPeriod = ref<Period>('Today')
+    const posts = computed(() => {
+      return [today, thisWeek, thisMonth].filter(post => {
+        if(currentPeriod.value === 'Today'){
+          return post.created.isAfter(moment().subtract(1, 'day'))
+        }
+
+        if(currentPeriod.value === 'This Week'){
+          return post.created.isAfter(moment().subtract(1, 'week'))
+        }
+
+        if(currentPeriod.value === 'This Month'){
+          return post.created.isAfter(moment().subtract(1, 'month'))
+        }
+
+        return false
+      })
+    })
+
     const setPeriod = (period: Period) => {
       currentPeriod.value = period
     }
     return {
+      posts,
       periods,
       setPeriod,
       currentPeriod
