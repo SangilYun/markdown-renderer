6cfec794a633559604aaa242797fbd67fe27ba62
yun <nasuler@gmail.com>
2021-08-12 23:56:52 +0900

    rename HelloWorld.vue to Timeline.vue

---
+++ src/components/Timeline.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="panel">
+    <span class="panel-tabs">
+      <a
+        v-for="period in periods"
+        :class="{'is-active': period === currentPeriod}"
+        @click="setPeriod(period)"
+      >
+        {{ period }}
+      </a>
+    </span>
+  </nav>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from 'vue';
+
+type Period = 'Today' | 'This Week' | 'This Month'
+export default defineComponent({
+  name: 'Timeline',
+  setup() {
+    const periods: Array<Period> = ['Today', 'This Week', 'This Month']
+    const currentPeriod = ref<Period>('Today')
+    const setPeriod = (period: Period) => {
+      currentPeriod.value = period
+    }
+    return {
+      periods,
+      setPeriod,
+      currentPeriod
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
