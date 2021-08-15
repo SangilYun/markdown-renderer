cd8d2d3b03ec4ace0fa881f2089406e18d28a0c6
yun <nasuler@gmail.com>
2021-08-15 15:49:25 +0900

    design a rough store structure. Keep it simple for now

---
+++ src/store.ts
@@ -0,0 +1,53 @@
+import {reactive, readonly} from "vue";
+import {Post} from "@/mocks";
+import axios from "axios";
+
+interface State {
+  posts: PostsState
+}
+
+interface PostsState {
+  ids: string[]
+  all: Map<string, Post>
+  loaded: boolean
+}
+
+class Store {
+  private state: State
+
+  constructor(initial: State) {
+    this.state = reactive(initial)
+  }
+
+  getState() {
+    return readonly(this.state)
+  }
+
+  async fetchPosts() {
+    const response = await axios.get<Post[]>('/post')
+    const postsState: PostsState = {
+      ids: [],
+      all: new Map,
+      loaded: true
+    }
+    for (const post of response.data) {
+      postsState.ids.push(post.id)
+      postsState.all.set(post.id, post)
+    }
+    this.state.posts = postsState
+  }
+}
+
+const store = new Store({
+  posts: {
+    all: new Map(),
+    ids: [],
+    loaded: false
+  }
+})
+
+//Very common convention to use prefix `use` in a reusable function.
+//It's often called composables
+export function useStore(){
+  return store
+}
