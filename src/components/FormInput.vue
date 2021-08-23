a003fe1784d8d1763621f258a0560644303e57c3
yun <nasuler@gmail.com>
2021-08-23 16:11:27 +0900

    Add FormInput

---
+++ src/components/FormInput.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="field">
+    <label class="label" :name="name"> {{ name }} </label>
+    <div class="control">
+      <input
+        :type="type"
+        class="input"
+        :value="modelValue"
+        @input="handleInput"
+      />
+    </div>
+    <p class="is-danger help">{{ error }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    modelValue: {
+      type: String,
+      required: true
+    },
+    error: {
+      type: String,
+
+    }
+  },
+  setup(props, ctx){
+    const handleInput = (e: Event) => {
+      ctx.emit('update:modelValue', (e.target as HTMLInputElement).value)
+    }
+    return {
+      handleInput
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
