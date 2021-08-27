3f12494bd2ca85c7a9a59b63255cdca77fb58831
yun <nasuler@gmail.com>
2021-08-27 11:39:34 +0900

    Exercise 2) add regression test for PostWriter.spec.ts
    mentioned in lecture 6.4 - 00:46

--- tests/unit/PostWriter.spec.ts
+++ tests/unit/PostWriter.spec.ts
@@ -1,5 +1,6 @@
 import PostWriter from "../../src/components/PostWriter.vue";
 import { mount } from "@vue/test-utils";
+import { Post, today } from "@/mocks";
 
 describe('PostWriter', () => {
   it('emits a save event with the new post', async (done) => {
@@ -29,8 +30,25 @@ describe('PostWriter', () => {
       expect(emitted.html).toBe('<h2 id="new-content">New content</h2>\n')
       done()
     }, 300)
-
-
   })
 
+  it('should render content provided as markdown prop', async (done) => {
+    const post: Post = {
+      ...today,
+      markdown: '__content__'
+    }
+    // @ts-ignore
+    const wrapper = mount(PostWriter, {
+      props: {
+        post
+      }
+    })
+
+    const title = wrapper.get<HTMLDivElement>('[data-testid="content"]').element.innerText
+    expect(title).toBe('__content__')
+    setTimeout(async () => {
+      expect(wrapper.html()).toContain('<strong>content</strong>')
+      done()
+    }, 300)
+  })
 });
